dd354159e32e0d7b5a7acca9efa2b7a37731eb1d
Georgiy Mikhailenko <kislball@yahoo.com>
2026-03-07 15:00:17 +0300

    WIP: implement graph printing

--- build/CMakeFiles/graphs.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for graphs.
-# This may be replaced when dependencies are built.
--- main.c
+++ main.c
@@ -1,8 +1,38 @@
 #include <stdio.h>
 #include "graph.h"
 
-int main()
+void graphPrint(Graph* graph) {
+    unsigned size = graphSize(graph);
+    
+    for (unsigned vertex = 0; vertex < size; vertex++) {
+	    bool err = false;
+	    VertexList neighbours = graphGetNeighbours(graph, vertex, &err);
+	    if (err) {
+		    fprintf(stderr, ":c\n");
+		    break;
+	    }
+	    printf("%d:", vertex);
+	    for (unsigned i = 0; i < neighbours.count; i++) {
+		    if (neighbours.vertices[i]) {
+			    printf(" %d", i); 
+		    }
+	    }
+	    printf("\n");
+    }
+}
+
+int main(int argc, char** argv)
 {
-    printf("Hello!\n");
-    return 0;
+	if (argc != 2) {
+		fprintf(stderr, "correct usage: %s <filename>\n", argv[0]);
+		return 1;
+	}
+	char* fileName = argv[1];
+	printf("before read\n");
+	Graph* graph = graphRead(fileName);
+	printf("after read\n");
+	printf("%d\n", graphSize(graph));
+	graphPrint(graph);
+	// graphFree(&graph);
+	return 0;
 }
